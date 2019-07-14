e7d0198f4b1281ee90522f99262f7d486f3692f3
Chris Mara <cm395@njit.edu>
2019-07-13 20:48:52 -0400

    Finish Base Jumbotron Preset

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,12 +6,14 @@
 <body>
 @include('includes.menu')
 
+<main role="main">
 @yield('content')
+</main>
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 
 @include("includes.jsfooter")
-
+@include("includes.footer")
 </body>
 </html>
