3c57e11cbdfff37942cba269b0678d40b70d1981
Chris Mara <cm395@njit.edu>
2019-07-13 20:34:16 -0400

    Initial Setup

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+@include("includes.head")
+
+<body>
+@include('includes.menu')
+
+@yield('content')
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+@include("includes.jsfooter")
+
+</body>
+</html>
